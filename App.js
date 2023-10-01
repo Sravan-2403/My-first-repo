7d934c24a60cd7e0bcaa424f473fd74e69f6ba7c
Sravan Thiparala <110044079+Sravan-2403@users.noreply.github.com>
2023-10-01 22:11:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import NavBar from "./component/NavBar/navbar";
 import Intro from "./component/Intro/intro";
-import Skill from "./component/Skill/skill";
+import Skills from "./component/Skills/skills";
+import Works from "./component/Works/works";
 
 function App() {
   return (
       <div className="App">
-        <NavBar />
-        <Intro />
-        <Skill />
+        <NavBar/>
+        <Intro/>
+        <Skills/>
+        <Works/>
     </div>
   );
 }
